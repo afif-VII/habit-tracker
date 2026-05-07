df5cbcff329aa965c36a158b67e625c37125fa9d
Afif <81876281+afif-VII@users.noreply.github.com>
2026-05-07 10:28:22 +0100

    Add /habits endpoint and move sample data into list

--- app.py
+++ app.py
@@ -2,10 +2,29 @@ from flask import Flask
 
 app = Flask(__name__)
 
+habits = [
+            {
+                "id": 1,
+                "name": "Reading",
+                "time": "1 hour a day",
+            },
+            {
+                "id": 2,
+                "name": "Gym",
+                "time": "1 hour a day",
+            }
+        ]
+
 @app.route("/")
 def home():
     return {
         "message": "Habit Tracker API is running"
+    }
+
+@app.route("/habits")
+def get_habits():
+    return {
+        "habits": habits
     }
 
 if __name__ == "__main__":
